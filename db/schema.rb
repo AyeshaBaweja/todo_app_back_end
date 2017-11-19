a6cedb5f3c125bb8bbb721d3b48c5380a6f872cf
Ayesha Baweja <ayeshabaweja650@gmail.com>
2017-11-19 18:54:13 +0000

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20171119175824) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string   "description"
     t.integer  "pomodoro_estimate"
